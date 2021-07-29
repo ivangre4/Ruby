baef6754694f959bdfe3d4ca320f986d9743b6ad
Ivan Ipofroditov <ivan.ipofroditov@gmail.com>
2021-07-29 08:20:35 +0300

    phone_to_number создал программу для перевода буквенного набора на клавиатуре телефона в цифровой

--- phone_to_number.rb
+++ phone_to_number.rb
@@ -1,13 +1,27 @@
 
 def phone_to_number(phone)
-arr1 = [
-    [], [], ["A","B","C"], ["D","E","F"], ["G", "H", "I"], ["J","K","L"], ["M","N","O"],
-    ["P","Q","R","S"], ["T","U","V"], ["W","X","Y","Z"]
-    ] 
-phone = phone.delete "0-9"
-puts phone
+    arr1 = [
+        ["0"], ["1"], ["2","A","B","C"], ["3","D","E","F"], ["4","G", "H", "I"], ["5","J","K","L"], ["6","M","N","O"],
+        ["7","P","Q","R","S"], ["8","T","U","V"], ["9","W","X","Y","Z"]
+        ] 
+    
+    puts
+        
+#Сначала перебираем каждый символ номера телефона, затем каждый массив массива. И во вложенных массивах ищем символ телефона, если символ есть -> печатаем индекс
+        phone.each_char do |char|
+            arr1.each do |array|
 
-
+                if array.include?(char)
+                number = arr1.index(array)
+                print number
+                end
+            
+            end
+        
+        end
+        
+    
 end
 
+print "ВВЕДИТЕ НОМЕР:"
 phone_to_number(gets.chomp)
