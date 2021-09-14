6f02922708a894f201e042ac02fa5bd955767a36
Ivan Ipofroditov <ivan.ipofroditov@gmail.com>
2021-09-14 21:14:57 +0300

    new tasks

--- test.rb
+++ test.rb
@@ -3,19 +3,13 @@ multy = -> (num1, num2) {return num1 * num2}
 pow = -> (num1, num2) {return num1 ** num2}
 
 def product(num1, num2, func)
-    result = 0
     # BEGIN (write your solution here)
-    if num1 == num2
-        result = func.call(num1, num2)
+
+    if num1 == num2 - 1
+        return func.call(num1, num2)
     else
-        loop do
-            result = result + func.call(num1, num1 + 1)
-            num1 += 1
-            puts result
-            break if num1 == num2
-        end
+        puts func.call(num1,num1 + 1) + product(num1, num2, func)
     end
-    puts result
     # END
 end
 
