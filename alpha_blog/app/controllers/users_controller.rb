334784e440389933f0dc57fff700e88a4ff677a6
Ivan Ipofroditov <ivan.ipofroditov@gmail.com>
2021-11-15 19:41:42 +0300

    add pagination and style it

--- alpha_blog/app/controllers/users_controller.rb
+++ alpha_blog/app/controllers/users_controller.rb
@@ -1,12 +1,12 @@
 class UsersController < ApplicationController
   
   def index
-    @users = User.all
+    @users = User.paginate(page: params[:page], per_page: 5)
   end
 
   def show
     set_user
-    @articles = @user.articles
+    @articles = @user.articles.paginate(page: params[:page], per_page: 5)
   end
 
   def new
