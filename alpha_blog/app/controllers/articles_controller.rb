eb48ddfdec336677cdb37c297f801e376778e274
Ivan Ipofroditov <ivan.ipofroditov@gmail.com>
2021-11-16 21:34:15 +0300

    restrict actions for article controller

--- alpha_blog/app/controllers/articles_controller.rb
+++ alpha_blog/app/controllers/articles_controller.rb
@@ -1,6 +1,8 @@
 class ArticlesController < ApplicationController
 
     before_action :set_article, only: [:show, :edit, :update, :destroy]
+    before_action :require_user, except: [:show, :index]
+    before_action :require_same_user, only: [:edit, :update, :destroy]
 
     def show
     end
@@ -49,5 +51,12 @@ class ArticlesController < ApplicationController
 
     def article_params
         params.require(:article).permit(:title, :description)
+    end
+
+    def require_same_user
+      if current_user != @article.user
+        flash[:alert] = "You can edit or delete only your own article"
+        redirect_to @article
+      end
     end
 end
