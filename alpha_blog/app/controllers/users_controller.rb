a65f511c9051e8797384f89b0d7516f3e29550f4
Ivan Ipofroditov <ivan.ipofroditov@gmail.com>
2021-11-16 16:10:39 +0300

    add helpers method in application controoler

--- alpha_blog/app/controllers/users_controller.rb
+++ alpha_blog/app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update]
   
   def index
     @users = User.paginate(page: params[:page], per_page: 5)
@@ -16,6 +17,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id
       flash[:notice] = "Welcome to Alpha Blog #{@user.username}, you are successfully signed up"
       redirect_to @user
     else
