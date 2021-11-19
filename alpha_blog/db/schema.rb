278908147806b660d625d1962dafef43a4c96a55
Ivan Ipofroditov <ivan.ipofroditov@gmail.com>
2021-11-19 20:08:37 +0300

    add admin to users db table

--- alpha_blog/db/schema.rb
+++ alpha_blog/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_13_183346) do
+ActiveRecord::Schema.define(version: 2021_11_19_170055) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 2021_11_13_183346) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string "password_digest"
+    t.boolean "admin", default: false
   end
 
 end
